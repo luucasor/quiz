5d78106fdba9d88181bec36b69ebfe1c8bfea728
luucasor <lucas@jornadatecnologia.com>
2023-06-24 16:27:00 -0300

    criando primeiro componente Enunciado

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,15 @@ export default function Home() {
     RespostaModel.errada('Azul'),
     RespostaModel.certa('Preta'),
   ])
+
   return (
-    <Questao valor={questaoTeste}/>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh'
+    }}>
+      <Questao valor={questaoTeste}/>
+    </div>
   )
 }
