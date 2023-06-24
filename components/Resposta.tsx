ba47ae605173befe3dbd412bd3864f1c7b4bb343
luucasor <lucas@jornadatecnologia.com>
2023-06-24 16:39:09 -0300

    criando componente Resposta parte2

--- components/Resposta.tsx
+++ components/Resposta.tsx
@@ -5,7 +5,7 @@ interface RespostaProps {
     valor: RespostaModel
     indice: number
     letra: string
-    corLetra: string
+    corFundoLetra: string
 }
 export default function Resposta(props: RespostaProps) {
     const resposta = props.valor
@@ -13,7 +13,10 @@ export default function Resposta(props: RespostaProps) {
         <div className={styles.resposta}>
             <div className={styles.conteudoResposta}>
                 <div className={styles.frente}>
-                    <div className={styles.letra}>
+                    <div className={styles.letra}
+                        style={{
+                            backgroundColor: props.corFundoLetra
+                        }}>
                         {props.letra}
                     </div>
                     <div className={styles.valor}>
