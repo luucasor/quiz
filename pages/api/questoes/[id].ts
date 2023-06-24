380101ba7c97df25341e8db488d97fd57ea33648
luucasor <lucas@jornadatecnologia.com>
2023-06-24 15:37:02 -0300

    método para responder questões

--- pages/api/questoes/[id].ts
+++ pages/api/questoes/[id].ts
@@ -3,6 +3,7 @@ import questoes from '@/pages/api/bancoDeQuestoes'
 export default (req, res) => {
     const idSelecionado = +req.query.id
     const unicaQuestaoOuNada = questoes.filter(questao => questao.id === idSelecionado)
+    
     if(unicaQuestaoOuNada.length === 1){
         const questaoSelecionada = unicaQuestaoOuNada[0].embaralharRespostas()
         res.status(200).json(questaoSelecionada.paraObjeto())
