b01fd77d90ca436156ecdb1b64a43a8b0df450e3
luucasor <lucas@jornadatecnologia.com>
2023-06-27 15:17:12 -0300

    adicionando resultado do quiz

--- pages/resultado.tsx
+++ pages/resultado.tsx
@@ -1,7 +1,19 @@
+import { useRouter } from "next/router"
+import styles from "@/styles/Resultado.module.css"
+
 export default function Resultado() {
+    const router = useRouter()
+    
+    const total = +router.query.total
+    const certas = +router.query.certas
+    const percentual = Math.round((certas / total) * 100)
+
     return (
-        <div>
+        <div className={styles.resultado}>
             <h1>Resultado</h1>
+            <div>{total}</div>
+            <div>{certas}</div>
+            <div>{`${percentual}%`}</div>
         </div>
     )
 }
