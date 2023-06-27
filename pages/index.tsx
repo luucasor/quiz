35a2200701dc137f326b0090d2bc797f3ab62647
luucasor <lucas@jornadatecnologia.com>
2023-06-27 12:38:27 -0300

    Adicionado método useEffect para buscar ids das questoes e questao em json

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Questionario from "@/components/Questionario"
 import QuestaoModel from "@/model/questao"
 import RespostaModel from "@/model/resposta"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const questaoMock = new QuestaoModel(1, 'Qual é a melhor cor?', [
   RespostaModel.errada('Verde'),
@@ -10,9 +10,32 @@ const questaoMock = new QuestaoModel(1, 'Qual é a melhor cor?', [
   RespostaModel.certa('Preta'),
 ])
 
+const BASE_URL = 'http://localhost:3000/api'
 
 export default function Home() {
-  const [questao, setQuestao] = useState(questaoMock)
+  const [idsDasQuestoes, setIdsDasQuestoes] = useState<number[]>([])
+  const [questao, setQuestao] = useState<QuestaoModel>(questaoMock)
+
+  async function carregarIdsDasQuestoes() {
+    const resp = await fetch(`${BASE_URL}/questionario`)
+    const idsDasQuestoes = await resp.json()
+    console.log(idsDasQuestoes)
+    setIdsDasQuestoes(idsDasQuestoes)
+  }
+
+  async function carregarQuestao(idQuestao: number) {
+    const resp = await fetch(`${BASE_URL}/questoes/${idQuestao}`)
+    const json = await resp.json()
+    console.log(json)
+  }
+
+  useEffect(() => {
+    idsDasQuestoes.length > 0 && carregarQuestao(idsDasQuestoes[0])
+  }, [idsDasQuestoes])
+
+  useEffect(() => {
+    carregarIdsDasQuestoes()
+  }, [])
 
   function questaoRespondida(questao: QuestaoModel) {
 
@@ -23,19 +46,11 @@ export default function Home() {
   }
 
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh'
-    }}>
       <Questionario 
         questao={questao}
         ultima={true}
         questaoRespondida={questaoRespondida}
         irPraProximoPasso={irPraProximoPasso}
       />
-    </div>
   )
 }
