13008940c5343b4c84ab2325e5e8800f7262e43c
luucasor <lucas@jornadatecnologia.com>
2023-06-28 16:11:59 -0300

    Adicionada variável para BASE_URL

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import QuestaoModel from "@/model/questao"
 import { useEffect, useState } from "react"
 import { useRouter } from "next/router"
 
-const BASE_URL = 'http://localhost:3000/api'
+const BASE_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/api`
 
 export default function Home() {
   const router = useRouter()
